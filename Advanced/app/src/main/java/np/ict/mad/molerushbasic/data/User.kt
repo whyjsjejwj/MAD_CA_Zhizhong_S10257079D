a9eeca1557555986f7959d1f83dadb0f61ea5cb2
whyjsjejwj <s10257079@connect.np.edu.sg>
2026-01-18 20:50:40 +0800

    Done with the Room DB

--- Advanced/app/src/main/java/np/ict/mad/molerushbasic/data/User.kt
+++ Advanced/app/src/main/java/np/ict/mad/molerushbasic/data/User.kt
@@ -1,2 +1,12 @@
 package np.ict.mad.molerushbasic.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val username: String,
+    val password: String,
+    val highScore: Int = 0
+)
--- Advanced/app/src/main/java/np/ict/mad/molerushbasic/data/UserDao.kt
+++ Advanced/app/src/main/java/np/ict/mad/molerushbasic/data/UserDao.kt
@@ -1,2 +1,17 @@
 package np.ict.mad.molerushbasic.data
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Insert
+    suspend fun register(user: User)
+
+    @Query("SELECT * FROM users WHERE username = :username LIMIT 1")
+    suspend fun getUser(username: String): User?
+
+    @Query("UPDATE users SET highScore = :newScore WHERE username = :username")
+    suspend fun updateScore(username: String, newScore: Int)
+}
